2dbd2409d9d6518ed21d05dc7415205d7abbd46e
vika <vikatekutova7@gmail.com>
2026-03-20 11:10:32 +0300

    Step 7: Setup NavHost and complete navigation

--- app/src/main/java/com/tekutova/student_planner/MainActivity.kt
+++ app/src/main/java/com/tekutova/student_planner/MainActivity.kt
@@ -7,10 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.tekutova.student_planner.navigation.StudentPlannerNavHost
 import com.tekutova.student_planner.ui.theme.Student_PlannerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +18,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Student_PlannerTheme {
+                val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    StudentPlannerNavHost(
+                        navController = navController,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +30,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Student_PlannerTheme {
-        Greeting("Android")
-    }
-}
